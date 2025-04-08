d1e8e3d56100c7b729d0be942814c1b3ec36229f
Dongmin Yu <ydm2790@gmail.com>
2025-04-08 20:31:58 +0900

    feat: export all necessary models and enums from portone_flutter_v2.dart
    This commit exports all necessary models and enums from the `portone_flutter_v2.dart` library. This change ensures that all required classes and enumerations are accessible when importing the library, providing a more convenient and complete API for developers using the PortOne Flutter V2 package.

--- lib/portone_flutter_v2.dart
+++ lib/portone_flutter_v2.dart
@@ -1,4 +1,19 @@
 /// A module for integrating PortOne V2 payment service in Flutter App.
-library;
+library portone_flutter_v2;
 
+export 'portone_flutter_v2.dart';
+export 'src/enums/cash_receipt_trade_option.dart';
+export 'src/enums/country.dart';
+export 'src/enums/payment_currency.dart';
+export 'src/enums/payment_locale.dart';
+export 'src/enums/payment_pay_method.dart';
+export 'src/enums/window_type.dart';
+export 'src/models/address.dart';
+export 'src/models/bypass/kakaopay_payment_bypass.dart';
+export 'src/models/bypass/tosspay_v2_payment_bypass.dart';
+export 'src/models/bypass/tosspayments_payment_bypass.dart';
+export 'src/models/customer.dart';
+export 'src/models/offer_period.dart';
+export 'src/models/payment_request.dart';
+export 'src/models/product_detail.dart';
 export 'src/port_one_payment.dart';
