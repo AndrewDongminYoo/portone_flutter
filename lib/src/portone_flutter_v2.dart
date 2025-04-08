7c67ed403093c9968f9b9e8062a19f92d194137e
Dongmin Yu <ydm2790@gmail.com>
2025-04-08 20:27:06 +0900

    feat: implement PortOnePayment widget using InAppWebView
    This commit introduces the `PortOnePayment` widget, which facilitates the PortOne v2 payment process within a Flutter application using an `InAppWebView`.
    
    The `PortOnePayment` widget:
    
    - Loads the PortOne browser SDK within an `InAppWebView`.
    - Accepts a `PaymentRequest` object to initialize the payment.
    - Handles navigation events, including redirects and custom schemes.
    - Provides callbacks for error handling and payment result processing.
    - Uses an `IndexedStack` to display a placeholder widget while the web view is loading, improving the user experience.
    
    The test file has been updated to include a group for `PortOnePayment`.

--- lib/src/portone_flutter_v2.dart
+++ lib/src/portone_flutter_v2.dart
@@ -1,7 +1,193 @@
-/// {@template portone_flutter_v2}
-/// A module for integrating PortOne V2 payment service in Flutter App.
-/// {@endtemplate}
-class PortoneFlutterV2 {
-  /// {@macro portone_flutter_v2}
-  const PortoneFlutterV2();
+// 🎯 Dart imports:
+import 'dart:convert';
+import 'dart:typed_data';
+
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+// 📦 Package imports:
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:logger/logger.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+// 🌎 Project imports:
+import 'package:portone_flutter_v2/src/models/payment_request.dart';
+
+/// A widget that implements the PortOne v2 payment process using a [InAppWebView].
+///
+/// This widget uses an [IndexedStack] to initially show a placeholder widget ([initialChild])
+/// instead of a blank white screen. Once the web page has been fully loaded (as indicated
+/// by the [InAppWebView]'s `onLoadStop` callback), the [IndexedStack] switches its view
+/// to display the actual web view.
+///
+/// The widget requires a [PaymentRequest] ([data]) to initialize the payment process using
+/// PortOne's browser SDK. Optionally, [onError] can be provided to handle errors, and [callback]
+/// will be invoked with query parameters when a payment result is received. A [Logger] may be
+/// passed for debugging purposes.
+///
+/// See also:
+///  - [InAppWebView] from flutter_inappwebview
+class PortOnePayment extends StatefulWidget {
+  /// Creates a [PortOnePayment] widget.
+  ///
+  /// The [data] parameter must be provided with the necessary payment information.
+  /// The [initialChild] is an optional widget that is displayed while the web view is loading.
+  const PortOnePayment({
+    required this.data,
+    super.key,
+    this.appBar,
+    this.onError,
+    this.callback,
+    this.logger,
+    this.initialChild,
+  });
+
+  /// Optional app bar to be displayed on top of the widget.
+  final PreferredSizeWidget? appBar;
+
+  /// Payment information used to start the payment process.
+  final PaymentRequest data;
+
+  /// Optional error callback which is invoked when a JavaScript error occurs.
+  final void Function(dynamic error)? onError;
+
+  /// Optional callback invoked with the payment result parameters.
+  final void Function(Map<String, String> result)? callback;
+
+  /// Optional logger for debugging.
+  final Logger? logger;
+
+  /// An optional widget to display while the web content is loading.
+  final Widget? initialChild;
+
+  @override
+  State<PortOnePayment> createState() => _PortOnePaymentState();
+}
+
+class _PortOnePaymentState extends State<PortOnePayment> {
+  static const handlerName = 'portoneError';
+
+  late InAppWebViewController? controller;
+
+  int _stackIndex = 0;
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final html = '''
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://cdn.portone.io/v2/browser-sdk.js"></script>
+<script>
+  window.addEventListener("flutterInAppWebViewPlatformReady", () => {
+    PortOne.requestPayment(${jsonEncode(widget.data.toJson())})
+      .catch((err) => window.flutter_inappwebview.callHandler("$handlerName", err.message));
+  });
+</script>
+</head>
+<body></body>
+</html>
+''';
+
+    return Scaffold(
+      appBar: widget.appBar,
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, Object? result) async {
+          if (!mounted) {
+            return;
+          }
+          if (controller != null && await controller!.canGoBack()) {
+            await controller!.goBack();
+          } else {
+            SchedulerBinding.instance.addPostFrameCallback((_) {
+              if (mounted && Navigator.of(context).canPop()) {
+                Navigator.of(context).pop();
+              }
+            });
+          }
+        },
+        child: SafeArea(
+          child: IndexedStack(
+            index: _stackIndex,
+            children: [
+              widget.initialChild ?? Container(),
+              InAppWebView(
+                initialSettings: InAppWebViewSettings(
+                  useShouldOverrideUrlLoading: true,
+                  resourceCustomSchemes: ['intent'],
+                ),
+                onWebViewCreated: (InAppWebViewController created) async {
+                  controller = created;
+                  controller!.addJavaScriptHandler(
+                    handlerName: handlerName,
+                    callback: (List<dynamic> data) async {
+                      widget.logger?.e(data.first.runtimeType);
+                      widget.onError?.call(data.first);
+                    },
+                  );
+                  await controller!.loadData(
+                    data: html,
+                    baseUrl: WebUri('https://flutter-sdk-content.portone.io/'),
+                  );
+                },
+                onLoadResourceWithCustomScheme: (InAppWebViewController controller, WebResourceRequest resource) async {
+                  return CustomSchemeResponse(contentType: 'text/html', data: Uint8List(0));
+                },
+                onLoadStop: (controller, Uri? url) async {
+                  if (mounted) {
+                    setState(() {
+                      _stackIndex = 1;
+                    });
+                  }
+                },
+                shouldOverrideUrlLoading: (InAppWebViewController controller, NavigationAction navigateAction) async {
+                  final url = navigateAction.request.url;
+                  widget.logger?.i('navigation action request uri: ${url!.uriValue}');
+                  switch (url!.uriValue.scheme) {
+                    case 'http':
+                    case 'https':
+                      return NavigationActionPolicy.ALLOW;
+                    case 'portone':
+                      widget.callback?.call(url.queryParameters);
+                      return NavigationActionPolicy.CANCEL;
+                    case 'intent':
+                      final uri = url.rawValue;
+                      final firstColon = uri.indexOf(':');
+                      final firstHash = uri.indexOf('#');
+                      final hashParams = uri.substring(firstHash + 1).split(';');
+                      String? scheme;
+                      for (final param in hashParams) {
+                        final keyValue = param.split('=');
+                        if (keyValue.elementAtOrNull(0) == 'scheme') {
+                          scheme = keyValue[1];
+                        }
+                      }
+                      final redirect = '$scheme${uri.substring(firstColon, firstHash)}';
+                      if (await canLaunchUrlString(redirect)) {
+                        await launchUrlString(redirect);
+                      }
+                      return NavigationActionPolicy.CANCEL;
+                    default:
+                      if (await canLaunchUrl(url.uriValue)) {
+                        await launchUrl(url.uriValue);
+                      }
+                      return NavigationActionPolicy.CANCEL;
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
--- test/src/portone_flutter_v2_test.dart
+++ test/src/portone_flutter_v2_test.dart
@@ -1,15 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
 // 📦 Package imports:
 import 'package:flutter_test/flutter_test.dart';
 
-// 🌎 Project imports:
-import 'package:portone_flutter_v2/portone_flutter_v2.dart';
-
 void main() {
-  group('PortoneFlutterV2', () {
-    test('can be instantiated', () {
-      expect(PortoneFlutterV2(), isNotNull);
-    });
-  });
+  group('PortOnePayment', () {});
 }
