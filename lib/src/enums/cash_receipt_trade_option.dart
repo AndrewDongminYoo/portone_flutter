8614e54b3fb8f4aeefde971fd70d4cf5e4e7d466
Dongmin Yu <ydm2790@gmail.com>
2025-04-09 22:35:38 +0900

    feat: convert CashReceiptTradeOption enum to use JsonEnum with value mapping
    This commit converts the `CashReceiptTradeOption` enum to use the `JsonEnum` annotation with explicit value mapping.
    
    The following changes were made:
    
    - Added `value` field to each enum value, representing the string value used in JSON serialization.
    - Updated the generated code to reflect the enum value changes.
    
    These changes improve the clarity and maintainability of the enum serialization and deserialization process.

--- lib/src/enums/cash_receipt_trade_option.dart
+++ lib/src/enums/cash_receipt_trade_option.dart
@@ -4,10 +4,11 @@ import 'package:json_annotation/json_annotation.dart';
 /// 현금영수증 발급 타입 enum
 @JsonEnum()
 enum CashReceiptTradeOption {
-  @JsonValue('CULTURE')
-  CULTURE,
-  @JsonValue('GENERAL')
-  GENERAL,
-  @JsonValue('PUBLIC_TP')
-  PUBLIC_TP,
+  culture('CULTURE'),
+  general('GENERAL'),
+  publicTp('PUBLIC_TP'),
+  ;
+
+  const CashReceiptTradeOption(this.value);
+  final String value;
 }
