a0562df44bda3969b9834f859ad86d554fbe774e
Dongmin Yu <ydm2790@gmail.com>
2025-04-10 14:51:13 +0900

    feat: export PGCompany enum
    This commit exports the `PGCompany` enum, making it accessible for use in other parts of the package.
    
    The following change was made:
    
    - Added `export 'pg_company.dart';` to the `lib/src/enums/enums.dart` file.
    
    This change allows other modules to utilize the `PGCompany` enum, which represents the supported Payment Gateway (PG) companies.

--- lib/src/enums/enums.dart
+++ lib/src/enums/enums.dart
@@ -3,5 +3,6 @@ export 'country.dart';
 export 'payment_currency.dart';
 export 'payment_locale.dart';
 export 'payment_pay_method.dart';
+export 'pg_company.dart';
 export 'product_type.dart';
 export 'window_type.dart';
