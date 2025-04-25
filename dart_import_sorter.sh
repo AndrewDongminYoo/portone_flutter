53cf58db788380c9401a45c576867f4dd2f9e98e
Dongmin Yu <ydm2790@gmail.com>
2025-04-25 18:39:21 +0900

    fix: correct exit code in dart_import_sorter.sh
    This commit fixes an issue in the `dart_import_sorter.sh` script where the exit code was inverted. The script now correctly exits with code 1 if files were sorted, and 0 otherwise.
    
    The following changes were made:
    
    - Corrected the if condition to exit with 1 when files are sorted and 0 when no files are sorted.

--- dart_import_sorter.sh
+++ dart_import_sorter.sh
@@ -3,8 +3,8 @@
 output=$(dart run import_sorter:main -e)
 
 # 정렬된 파일 수를 파싱합니다.
-sorted=$(echo "$output" | grep -Eo "Sorted [0-9]+ files" | grep -Eo "[0-9]+")
-if [[ $sorted -gt 0 ]]; then
+sorted=$(echo "${output}" | grep -Eo "Sorted [0-9]+ files" | grep -Eo "[0-9]+")
+if [[ ${sorted} -gt 0 ]]; then
 	exit 1
 else
 	exit 0
